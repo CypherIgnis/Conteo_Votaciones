0dec2b4d2f6f23c04820cab0f65b523e8957822d
CypherIgnis <116596423+CypherIgnis@users.noreply.github.com>
2022-11-17 09:56:07 -0300

    Update conteo_votaciones.py

--- conteo_votaciones.py
+++ conteo_votaciones.py
@@ -1,1 +1,29 @@
+class Votations():
+
+    def __init__(self):
+        print("Comenzando programa")
+        self.candidate=candidate
+        self.party=party
+        self.counting=counting
+
+    def show_candidate(self):
+        print(self.candidato)
+
+    def show_party(self):
+        print(self.partido)
+
+    def show_counting(self):
+        print(self.conteo)
+        
+    def candidate(self,candidato):
+        return self.candidato
+     
+    def party(self,partido):
+        return self.partido
+        
+    def counting(self,conteo):
+        return self.conteo
+      
+a=candidate("Piñera")
+print(a.candidato)
 
